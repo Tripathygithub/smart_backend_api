698ab52eea9c738fff8ce0d27846de5dcab27f5f
Tripathygithub <98731218+Tripathygithub@users.noreply.github.com>
2024-12-30 15:45:50 +0530

    Update movie.js
    add extra movie

--- movie.js
+++ movie.js
@@ -4,7 +4,8 @@ let movies=[
   { id: 1, title: 'The Shawshank Redemption', director: 'Frank Darabont' },
   { id: 2, title: 'The Godfather', director: 'Francis Ford Coppola' },
   { id: 3, title: 'The Dark Knight', director: 'Christopher Nolan' },
-  { id: 4, title: 'Pulp Fiction', director: 'Quentin Tarantino' }
+  { id: 4, title: 'Pulp Fiction', director: 'Quentin Tarantino' },
+  { id: 5, title: 'The king', director: 'Soham Tripathy' }
 ];
 
  function getAllMovies(){
